6b3b7ebc73fd6ae9077929ca960ab51a1d237e67
Erikas P <erikaspipas99@gmail.com>
2025-10-07 19:50:23 +0300

    UpdateMachineFormList

---
+++ my-app/src/MachineForm.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+
+export const MachineForm = () => {
+  const [machine, setMachine] = useState({
+    _id: "",
+    adresses: "",
+    ip: "",
+  });
+  const [machineList, setMachineList] = useState([]);
+
+  const fetchMachines = async () => {
+    try {
+      const result = await fetch("http://localhost:3000/machine");
+      if (!result.ok) {
+        result.send("Failed to get fetch info");
+      }
+      const data = await result.json();
+      setMachineList(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetchMachines();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/machine", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(machine),
+      });
+      if (!response.ok) {
+        console.log("Bad info");
+        return;
+      }
+      const data = await response.json();
+      console.log("Done");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleChange = (e) => {
+    setMachine({ ...machine, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <h2>New Create Machine</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          _id="text"
+          adresses="name"
+          value={machine.adresses}
+          onChange={handleChange}
+          placeholder="Machine name"
+        />
+        <button type="submit">ADD machine</button>
+      </form>
+
+      <h2>Machine List</h2>
+      <ul>
+        {machineList.map((machines) => {
+          <li key={machines._id}></li>;
+        })}
+      </ul>
+    </div>
+  );
+};
