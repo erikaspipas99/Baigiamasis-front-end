ac86d51b4094596b482fb54ea9e12d6865ca12bc
Erikas P <erikaspipas99@gmail.com>
2025-10-09 18:21:19 +0300

    Created_token

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,7 +6,13 @@ import { MachineForm } from "./MachineForm";
 import { MachineList } from "./MachineList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    if (savedToken) {
+      setToken(savedToken);
+    }
+  }, []);
 
   return (
     <>
